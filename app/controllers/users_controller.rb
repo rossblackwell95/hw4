fae4273a813ed07ccb2f03b31f7851a5a673ac9f
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-03-18 03:01:43 +0000

    Encrypt Password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
   def new
   end
 
+  require 'bcrypt'
+
   def create
     @user = User.new
     @user["first_name"] = params["first_name"]
     @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
     redirect_to "/"
   end
