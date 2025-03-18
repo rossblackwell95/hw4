bfb4b3761acecbeaa5fefaf0b807e15dacd905ce
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-03-18 03:37:18 +0000

    Restrict Access to Authenticated User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    @current_user = User.find_by(id: session["user_id"])
+  end
+
+  def authenticate_user
+    if @current_user.nil?
+      flash["notice"] = "Please login."
+      redirect_to "/login"
+    end
   end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user
 
   def index
     @places = Place.all
