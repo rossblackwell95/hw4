d2a8113d1ee8c03679369000b9d7983d0e4a571b
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-03-18 03:07:02 +0000

    Create Login Routing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+   
+     if @user && BCrypt::Password.new(@user["password"]) == params["password"]
+       session["user_id"] = @user["id"]
+       flash["notice"] = "Login Successful"
+       redirect_to "/places"
+     else
+       flash["notice"] = "Invalid Login Credentials. Please Try Again."
+       redirect_to "/login"
+     end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
   resources "entries"
   resources "places"
   resources "sessions"
